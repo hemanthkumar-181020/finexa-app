8b33d797e75b8e1b83953009b83407d92c879c0d
santhosh <santhoshvarikuti130706@gmail.com>
2025-12-13 21:27:10 +0530

    Update login file

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,153 @@
-import { View, Text } from "react-native";
-
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Login() {
+  type FormErrors = {
+  username?: string;
+  password?: string;
+};
+
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const validateform=()=>{
+    let errors: FormErrors = {};
+
+    if(username==="") errors.username="username is required";
+    if(password==="") errors.password="password is required";
+    setErrors(errors);
+    return Object.keys(errors).length===0;
+
+  }
+  const submit=()=>{
+    if(validateform())
+    {
+      console.log("usetrname and password are accepted");
+      setUsername("");
+      setPassword("");
+      setErrors({});
+    }
+    else{
+      console.log("details are not correct");
+    }
+
+    
+  }
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+        <SafeAreaView style={styles.sav}>
+          <View style={styles.c1}>
+            <Text style={styles.ht}>Welcome Back</Text>
+            <Text style={styles.Text}>Know Your Habits. Master Your Money</Text>
+          </View> 
+          <View style={styles.c2}>
+                <View style={styles.cmp}>
+                  <Text style={styles.Text}>Username</Text>
+                <TextInput placeholder="john123" style={styles.ti} value={username} onChangeText={setUsername} autoCapitalize="none"
+/>
+               
+                </View>
+                {errors.username && <Text style={{ color: 'red' }}>{errors.username}</Text>}
+
+                <View style={styles.cmp}>
+                  <Text style={styles.Text}>Password</Text>
+                  <TextInput placeholder="********" secureTextEntry={true} style={styles.ti} value={password} onChangeText={setPassword} autoCapitalize="none"/>
+                </View>
+                {errors.password && <Text style={{ color: 'red' }}>{errors.password}</Text>}
+          </View>
+          
+          <View style={styles.c3} >
+            
+               
+               <Pressable style={styles.button} onPress={submit}>
+                  <Text style={styles.btn}>Login</Text>
+                </Pressable>
+          </View>
+          
+          <View  style={styles.c4}>
+            <Text>___________ or continue with ____________</Text>
+            <Pressable style={styles.button}>
+                  <Text style={styles.btn}>Google</Text>
+                </Pressable>
+            <Text>doesn't have an account?<Link href="/signup"><Text style={{ color: 'blue' }}>sign up</Text> </Link></Text>
+          </View>
+
+        </SafeAreaView>
     </View>
-  );
+  )
 }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+    },
+    sav: { 
+      flex: 1 ,
+      backgroundColor:'violet',
+      gap:20,
+     },
+     c1:{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+
+     },
+     c2:{
+       flex:2,
+       alignItems:"flex-start",
+       marginLeft:"10%",
+     },
+     c3:{
+       flex:1,
+       alignItems:"center",
+     },
+     c4:{
+       flex:1,
+       alignItems:"center",
+     },
+    ti:{
+      borderRadius:15,
+      borderWidth:1,
+      borderColor:'white',
+      padding:12,
+      marginBottom:16,
+      width:"88%",
+    },
+    Text:{
+      color:'white',
+      fontSize:15,
+    },
+    ht:{
+      color:'white',
+      fontSize:25,
+    },
+    bdy:{
+      flex:5,
+      gap:10,
+      alignItems:"flex-start",
+      paddingLeft:"10%",
+    },
+    cmp:{
+      flex:1,
+      width:"100%",
+      gap:5,  
+    },
+    btn:{
+      backgroundColor:'white',
+      color:'black',
+      padding:15,
+      borderRadius:30,
+      textAlign:'center',
+      fontSize:18,
+      fontWeight:'bold',
+      width:"100%",
+      
+    },
+    button:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      width:"80%",
+    },
+});
