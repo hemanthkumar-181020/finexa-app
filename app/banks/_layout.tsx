21fe5cf3d43c9c044bf706999bb62615bc13102a
santhosh <santhoshvarikuti130706@gmail.com>
2025-12-23 22:01:21 +0530

    added features to transactions

--- app/banks/_layout.tsx
+++ app/banks/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from 'expo-router';
 export default function bankLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="banks/phonepay" />
-      <Stack.Screen name="banks/sbi" />
-      <Stack.Screen name="banks/icici" />
+      <Stack.Screen name="phonepe" />
+      <Stack.Screen name="sbi" />
+      <Stack.Screen name="icici" />
     </Stack>
   );
 }
